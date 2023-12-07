89409c3e344a3261dd2026178ba3650e16e1b471
ella13johnson <ella13johnson@gmail.com>
2023-12-06 23:23:37 -0700

    Update exploratory_analysis.R

--- scripts/exploratory_analysis.R
+++ scripts/exploratory_analysis.R
@@ -1,10 +1,18 @@
 pacman::p_load(tidyverse, estimatr, broom, summarytools, fixest, binsreg)
 library(usmap)
 library(ggplot2)
-library(sf)
-library(tmap)
 library(RColorBrewer)
 
+# histogram
+ggplot(attendance_merged, aes(attendance)) +
+  geom_histogram(color = "#000000", fill = "#1f78b4") +
+  geom_vline(aes(xintercept = mean(attendance)), color = "#000000", size = 1.25) +
+  geom_vline(aes(xintercept = mean(attendance) + sd(attendance)), color = "#000000", size = 1, linetype = "dashed") +
+  geom_vline(aes(xintercept = mean(attendance) - sd(attendance)), color = "#000000", size = 1, linetype = "dashed") + labs(
+    title = "Histogram of Attendance of Religious Services (2005-2021)",
+    x = "Number of Services Attended in Past Year", y="") + theme(axis.title.y=element_blank(),
+                                                                  axis.text.y=element_blank(),
+                                                                  axis.ticks.y=element_blank()) + theme(panel.background = element_rect(colour = "black")) + theme(panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank())
 # create map of attendance
 mean_attend <- aggregate(attendance_merged$attendance, list(attendance_merged$state), FUN=mean)
 mean_attend <- mean_attend |>
@@ -13,7 +21,7 @@ mean_attend <- mean_attend |>
     average_attendance = x
   )
 mean_attend$average_attendance <- as.numeric(mean_attend$average_attendance)
-plot_usmap(data=mean_attend, values="average_attendance") + scale_fill_continuous("Number of Religious Services Attended") + theme(legend.position = "right" ) + labs(title = "Average Yearly Attendance of Religious Services (2005-2021)")
+plot_usmap(data=mean_attend, values="average_attendance") + scale_fill_continuous("Number of Religious Services Attended") + theme(legend.position = "right" ) + labs(title = "Average Yearly Attendance of Religious Services (2005-2021)") + theme(panel.background = element_rect(colour = "black"))
 # create map of percent regularly attended
 mean_reg_attend <- aggregate(attendance_merged$reg_attend, list(attendance_merged$state), FUN=mean)
 mean_reg_attend <- mean_reg_attend |>
@@ -52,9 +60,9 @@ event_study_untreat <- attendance_merged |>
   group_by(time) |>
   mutate(meanattendance = mean(attendance))
 
-legend_colors <- c("Expanded Medicaid" = "blue", "Did Not Expand Medicaid" = "red")
+legend_colors <- c("Expanded Medicaid" = "#1f78b4", "Did Not Expand Medicaid" = "#b2df8a")
 
-ggplot() + geom_line(data=event_study_treat,aes(x=time,y=meanattendance,color="Expanded Medicaid")) + geom_line(data=event_study_untreat,aes(x=time,y=meanattendance,color="Did Not Expand Medicaid")) + labs(x="Year (Relative to 2014)", y="Average Yearly Attendance", color = "Legend") + scale_color_manual(values = legend_colors) + ggtitle("Event Study") + theme_classic() + geom_vline(xintercept=0)
+ggplot() + geom_line(data=event_study_treat,aes(x=time,y=meanattendance,color="Expanded Medicaid")) + geom_line(data=event_study_untreat,aes(x=time,y=meanattendance,color="Did Not Expand Medicaid")) + labs(x="Year (Relative to 2014)", y="Average Yearly Attendance", color = "Expansion Status") + scale_color_manual(values = legend_colors) + ggtitle("Event Study") + theme_classic() + geom_vline(xintercept=0)
 
 #attendance overtime
 attendance_overtime <- attendance_merged |>
@@ -85,7 +93,7 @@ medicaid_percent_change_final <- rbind(medicaid_percent_change_before, medicaid_
 
 medicaid_percent_change_final <- medicaid_percent_change_final |>
   mutate(ln_enrollment = log(as.numeric(Enrollment)))
-  
+
 new_plot <- ggplot(data=medicaid_percent_change_final, aes(x=ln_enrollment,y=State)) + geom_line(aes(group=State)) + geom_point(size=2, aes(color = factor(year))) + scale_x_discrete(limits=10:17, breaks=c(11, 12, 13, 14, 15, 16, 17), labels = scales::comma_format(big.mark = ',', decimal.mark = '.')) + ggtitle("Medicaid Enrollment Before and After Expansion by State")
 legend_colors <- c("2013" = "#1f78b4", "2023" = "#b2df8a")
 new_plot + theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) + labs(x="Log(Medicaid Enrollment)", y="State", color = "Year") + scale_color_manual(values = legend_colors)
